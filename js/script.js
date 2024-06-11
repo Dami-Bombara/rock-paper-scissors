e7dd90af4c606204fb2c96c299425649adb632e2
Dami-Bombara <bombaradamian@gmail.com>
2024-06-11 17:34:31 +0200

    Add status of the game

--- js/script.js
+++ js/script.js
@@ -19,6 +19,12 @@ const scissorsButton = document.querySelector("#scissors");
 scissorsButton.addEventListener("click", () => humanChoice = choices[2]);
 scissorsButton.addEventListener("click", function(){ playRound(getComputerChoice(), humanChoice); });
 
+const humanPoints = document.querySelector("#human-score");
+const computerPoints = document.querySelector("#pc-score");
+
+const humanDecision = document.querySelector("#human-choice");
+const computerDecision = document.querySelector("#pc-choice");
+
 //************************************************************************* */
 
 // Game logic //
@@ -27,7 +33,14 @@ function getComputerChoice() {
     return choices[number];
 }
 
+function checkScore(callback) {
+    if (computerScore == 5 || humanScore == 5) console.log("Game over!");
+    callback();
+}
+
 function playRound(computerChoice, humanChoice) {
+    checkScore(evaluate);
+
     if (computerChoice == humanChoice) {
         console.log("Draw!");
     }
@@ -56,14 +69,23 @@ function playRound(computerChoice, humanChoice) {
         humanScore++;
         console.log("You win scissors beats paper!");
     }
+
+    computerDecision.textContent = "PC's choice: " + computerChoice;
+    humanDecision.textContent = "Your choice: " + humanChoice;
+
+    computerPoints.textContent = "PC's score: " + computerScore;
+    humanPoints.textContent = "Your score: " + humanScore;
+}
+
+console.log("\n");
+
+function evaluate() {
     console.log("Computer choice: " + computerChoice);
     console.log("Human choice: " + humanChoice);
     console.log("\n");
     console.log("Human score: " + humanScore);
     console.log("Computer score: " + computerScore);
 }
-
-console.log("\n");
 
 /*while (humanScore != 5 || computerScore != 5) {
     if (humanChoice != 0) {
