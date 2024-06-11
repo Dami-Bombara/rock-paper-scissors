114a958440c1642351ee3e0b79d2bf9f6f464654
Dami-Bombara <bombaradamian@gmail.com>
2024-06-11 16:27:35 +0200

    Add listeners to buttons to start a round

--- js/script.js
+++ js/script.js
@@ -3,13 +3,28 @@ let choices = ["rock", "paper", "scissors"]
 let computerScore = 0;
 let humanScore = 0;
 
+let computerChoice = 0;
+let humanChoice = 0;
+
+// DOM Manipulation //
+const rockButton = document.querySelector("#rock");
+rockButton.addEventListener("click", () => humanChoice = choices[0]);
+rockButton.addEventListener("click", function(){ playRound(getComputerChoice(), humanChoice); });
+
+const paperButton = document.querySelector("#paper");
+paperButton.addEventListener("click", () => humanChoice = choices[1]);
+paperButton.addEventListener("click", function(){ playRound(getComputerChoice(), humanChoice); });
+
+const scissorsButton = document.querySelector("#scissors");
+scissorsButton.addEventListener("click", () => humanChoice = choices[2]);
+scissorsButton.addEventListener("click", function(){ playRound(getComputerChoice(), humanChoice); });
+
+//************************************************************************* */
+
+// Game logic //
 function getComputerChoice() {
     number = Math.floor(Math.random() * 3);
     return choices[number];
-}
-
-function getHumanChoice() {
-    return prompt("Choose: Rock, paper, scissors.").toLowerCase();
 }
 
 function playRound(computerChoice, humanChoice) {
@@ -41,21 +56,26 @@ function playRound(computerChoice, humanChoice) {
         humanScore++;
         console.log("You win scissors beats paper!");
     }
-}
-
-console.log("\n");
-let computerChoice = 0;
-let humanChoice = 0;
-
-for (let i = 0; i <= 4; i++) {
-    computerChoice = getComputerChoice();
-    humanChoice = getHumanChoice();
-
-    playRound(computerChoice, humanChoice);
-
     console.log("Computer choice: " + computerChoice);
     console.log("Human choice: " + humanChoice);
     console.log("\n");
     console.log("Human score: " + humanScore);
     console.log("Computer score: " + computerScore);
 }
+
+console.log("\n");
+
+/*while (humanScore != 5 || computerScore != 5) {
+    if (humanChoice != 0) {
+        computerChoice = getComputerChoice();
+
+        playRound(computerChoice, humanChoice);
+
+        console.log("Computer choice: " + computerChoice);
+        console.log("Human choice: " + humanChoice);
+        console.log("\n");
+        console.log("Human score: " + humanScore);
+        console.log("Computer score: " + computerScore);
+        humanChoice = 0;
+    }
+}*/
